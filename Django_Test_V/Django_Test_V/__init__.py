bfe5c13ad94a214b9b172207a775d75612391a30
Prashant Shrestha <prashant.shrestha@dghub.io>
2022-05-05 12:05:24 +0545

    new files

---
+++ Django_Test_V/Django_Test_V/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for Django_Test_V.
+"""
---
+++ Django_Test_V/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\Projects\MyProject\Test Project\Django_Test_V\Django_Test_V\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
